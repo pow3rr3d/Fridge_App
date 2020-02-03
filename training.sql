2fca6be2727c0394c9c0e50dfb025f7d8671bb62
AMESSAGER <adrien.messager.diaz@bellvision.fr>
2020-02-03 09:39:20 +0100

    Update Files

--- training.sql
+++ training.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 22 jan. 2020 à 16:16
+-- Généré le :  lun. 03 fév. 2020 à 08:37
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.3.5
 
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `produit` (
   `produit` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=91 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `produit`
@@ -44,17 +44,63 @@ INSERT INTO `produit` (`id`, `produit`, `image`) VALUES
 (5, 'Fromage', 'Media/fromage.jpg'),
 (40, 'Oeuf', 'Media/Oeuf.jpg'),
 (29, 'Jambon', 'Media/Jambon.png'),
-(28, 'Beurre', ''),
-(30, 'Lait', ''),
-(34, 'Steack', ''),
+(41, 'Rape', ''),
+(34, 'Steack', 'media/Steack.jpg'),
 (33, 'Saumon', ''),
 (32, 'Huile', ''),
 (31, 'Pain', ''),
-(35, 'Sel', ''),
-(36, 'Poivre', ''),
 (37, 'Coca', ''),
 (38, 'Pates', ''),
-(39, 'Riz', '');
+(39, 'Riz', ''),
+(45, 'Steack Hache', ''),
+(42, 'ChÃ¨vre', ''),
+(43, 'Jambon cru', ''),
+(44, 'Jambon sec', ''),
+(46, 'Pommes de terre', ''),
+(47, 'Vignaire', ''),
+(48, 'Sel', ''),
+(49, 'Poivre', ''),
+(50, 'Eau minerale', ''),
+(51, 'Eau Gazeuse', ''),
+(52, 'Ketchup', ''),
+(53, 'Mayonnaise', ''),
+(54, 'Moutarde', ''),
+(55, 'Pain de mie', ''),
+(56, 'Pain', ''),
+(57, 'Baguette', ''),
+(58, 'Colin', ''),
+(59, 'Cabillot', ''),
+(60, 'Farine', ''),
+(61, 'Sucre (morceaux)', ''),
+(62, 'Sucre (poudre)', ''),
+(63, 'Cereales', ''),
+(64, 'Saucisson', ''),
+(65, 'Raviolli', ''),
+(66, 'Pizza', ''),
+(67, 'Creme fraiche', ''),
+(68, 'Beurre', ''),
+(69, 'Bouillon', ''),
+(70, 'Pommes chips', ''),
+(71, 'Bretzels', ''),
+(72, 'Glace', ''),
+(73, 'Cookies', ''),
+(74, 'Brioches', ''),
+(75, 'Madeleines', ''),
+(76, 'Gateau', ''),
+(77, 'Boeuf', ''),
+(78, 'Volaille', ''),
+(79, 'Jambon', ''),
+(80, 'Fruits de mer', ''),
+(81, 'Charcuterie', ''),
+(82, 'Boissons gazeuses', ''),
+(83, 'Jus', ''),
+(84, 'Cafe', ''),
+(85, 'The', ''),
+(86, 'Oranges', ''),
+(87, 'Pommes', ''),
+(88, 'Bananes', ''),
+(89, 'Laitue', ''),
+(90, 'Tomates', '');
 
 -- --------------------------------------------------------
 
@@ -73,7 +119,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `avatar` varchar(255) NOT NULL,
   `privileges` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `user`
@@ -82,7 +128,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 INSERT INTO `user` (`id`, `name`, `surname`, `email`, `emailConfirm`, `password`, `avatar`, `privileges`) VALUES
 (1, 'ADN', 'Powerred', 'powerred@gmail.com', 'Yes', '$argon2i$v=19$m=1024,t=2,p=2$bUhHcDYuWHhoOVNlWW1sWA$L9Cv/GKRgePvsDgMgWliCayPY1Ba18TcyQgiPcEaqv0', 'media/uploads/avatar_1.jpg', 'Admin'),
 (3, 'test', 'test', 'test@test.com', 'Yes', '$argon2i$v=19$m=1024,t=2,p=2$N2VTVGV2dmFxaXNtekU0ZA$oBqiIW9uXPFbt6PW4OyELV5s2MUsTGGeJhTVrT4iYfg', 'Media/Uploads/Avatar_3.PNG', 'User'),
-(7, 'test', 'test', 'email@email.com', 'Yes', '$argon2i$v=19$m=1024,t=2,p=2$Zkd2SGx2UDUxNzhXYWUwWA$wyHYDmOF9mRuSxpACFfyIKWfjjeie6cEezVQHn6qopg', '', 'User');
+(8, 'THE', 'AGREGRATOR', 'greg@a-gregator.fr', '', '$argon2i$v=19$m=1024,t=2,p=2$bUhHcDYuWHhoOVNlWW1sWA$L9Cv/GKRgePvsDgMgWliCayPY1Ba18TcyQgiPcEaqv0', 'media/uploads/avatar_8.png', 'Admin');
 
 -- --------------------------------------------------------
 
@@ -100,28 +146,14 @@ CREATE TABLE IF NOT EXISTS `user_produit` (
   `produit_img` varchar(255) NOT NULL,
   PRIMARY KEY (`joined_id`),
   KEY `FK_USER_ID` (`user_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=69 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=72 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `user_produit`
 --
 
 INSERT INTO `user_produit` (`joined_id`, `user_id`, `produit_id`, `produit_stock_reel`, `produit_stock_opti`, `produit_img`) VALUES
-(1, 1, 5, 0, 5, 'media/uploads/Fromage_1.jpg'),
-(35, 1, 8, 4, 4, ''),
-(19, 1, 29, 2, 5, ''),
-(59, 1, 33, 1, 2, ''),
-(66, 1, 38, 1, 2, ''),
-(45, 1, 30, 2, 2, 'media/uploads/Lait_1.jpg'),
-(57, 1, 35, 1, 2, 'media/uploads/Sel_1.jpg'),
-(36, 3, 5, 4, 2, ''),
-(37, 3, 40, 4, 2, ''),
-(38, 1, 40, 3, 3, ''),
-(39, 7, 5, 4, 2, ''),
-(40, 7, 34, 4, 2, ''),
-(60, 1, 37, 2, 2, ''),
-(68, 1, 34, 5, 10, ''),
-(67, 1, 39, 2, 2, '');
+(1, 1, 5, 1, 5, 'media/uploads/Fromage_1.jpg');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
