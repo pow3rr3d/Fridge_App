a372b89f13b24616165d62010a306f07fc27cbbf
AMESSAGER <adrien.messager.diaz@bellvision.fr>
2020-07-01 17:32:26 +0200

    User dashboard UX/UI rebuild

--- var/cache/dev/App_KernelDevDebugContainer.php
+++ var/cache/dev/App_KernelDevDebugContainer.php
@@ -16,6 +16,6 @@ if (!\class_exists(App_KernelDevDebugContainer::class, false)) {
 
 return new \Container1jjcUKh\App_KernelDevDebugContainer([
     'container.build_hash' => '1jjcUKh',
-    'container.build_id' => '2a9309de',
-    'container.build_time' => 1592316988,
+    'container.build_id' => 'fe6c4cd4',
+    'container.build_time' => 1593615464,
 ], __DIR__.\DIRECTORY_SEPARATOR.'Container1jjcUKh');
